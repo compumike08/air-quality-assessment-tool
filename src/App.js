15a98c8d87b4c7e697d9fac1d1d6134b1b0f23c9
compumike08 <compumike08@users.noreply.github.com>
2021-11-30 18:50:49 -0500

    Add dependancies semantic-ui-react & semantic-ui-css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 function App() {
